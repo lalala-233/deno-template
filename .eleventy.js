bb6bf15b9f1e5e479b4bc3907fd1f1877e1f53e2
lalala_233 <lalala_233@qq.com>
2026-06-14 16:37:06 +0800

    feat: generate all-in-one minified html

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,32 @@
-import { renderTodoItem } from './src/_includes/components/todo-item.ts'
+import { minify } from "html-minifier-terser";
 
-export default function(eleventyConfig) {
-  eleventyConfig.addShortcode('todoItem', (text, checked) =>
-    renderTodoItem({ text, checked: checked !== 'false' && !!checked })
-  )
-
+export default function (eleventyConfig) {
+  // minifyCss breaks css if tailwind css don't minify
+  // but gives little benefit if tailwind css minify
+  // minifyJs gives no benefit.
+  eleventyConfig.addTransform("minifyHtml", async (content, outputPath) => {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return await minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+      });
+    }
+    return content;
+  });
+  eleventyConfig.ignores.add("node_modules");
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
-      includes: '_includes',
+      input: ".",
+      output: "dist",
+      includes: "src/_includes",
     },
-    templateFormats: ['html', 'njk'],
-  }
+    templateFormats: ["html"],
+  };
 }
